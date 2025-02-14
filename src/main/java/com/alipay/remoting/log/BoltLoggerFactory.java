ac66d948e09e2d76763169b0ec20dff9f0d2c5b6
Nipuru <2789129195@qq.com>
2025-02-14 11:23:23 +0800

    修改 BoltLoggerFactory 使用 Sl4fj 默认工厂

--- src/main/java/com/alipay/remoting/log/BoltLoggerFactory.java
+++ src/main/java/com/alipay/remoting/log/BoltLoggerFactory.java
@@ -16,12 +16,9 @@
  */
 package com.alipay.remoting.log;
 
-import java.io.File;
-
 import org.slf4j.Logger;
 
-import com.alipay.remoting.util.StringUtils;
-import com.alipay.sofa.common.log.LoggerSpaceManager;
+import org.slf4j.LoggerFactory;
 
 /**
  * Customized logger factory
@@ -32,46 +29,6 @@ import com.alipay.sofa.common.log.LoggerSpaceManager;
  * @version $Id: BoltLoggerFactory.java, v 0.1 2017-09-05 16:06 tsui Exp $
  */
 public class BoltLoggerFactory {
-    public static final String  BOLT_LOG_SPACE_PROPERTY   = "bolt.log.space";
-
-    private static String       BOLT_LOG_SPACE            = "com.alipay.remoting";
-
-    private static final String LOG_PATH                  = "logging.path";
-    private static final String LOG_PATH_DEFAULT          = System.getProperty("user.home")
-                                                            + File.separator + "logs";
-    private static final String CLIENT_LOG_LEVEL          = "com.alipay.remoting.client.log.level";
-    private static final String CLIENT_LOG_LEVEL_DEFAULT  = "INFO";
-    private static final String CLIENT_LOG_ENCODE         = "com.alipay.remoting.client.log.encode";
-    private static final String COMMON_ENCODE             = "file.encoding";
-    private static final String CLIENT_LOG_ENCODE_DEFAULT = "UTF-8";
-
-    static {
-        String logSpace = System.getProperty(BOLT_LOG_SPACE_PROPERTY);
-        if (null != logSpace && !logSpace.isEmpty()) {
-            BOLT_LOG_SPACE = logSpace;
-        }
-
-        String logPath = System.getProperty(LOG_PATH);
-        if (StringUtils.isBlank(logPath)) {
-            System.setProperty(LOG_PATH, LOG_PATH_DEFAULT);
-        }
-
-        String logLevel = System.getProperty(CLIENT_LOG_LEVEL);
-        if (StringUtils.isBlank(logLevel)) {
-            System.setProperty(CLIENT_LOG_LEVEL, CLIENT_LOG_LEVEL_DEFAULT);
-        }
-
-        String commonEncode = System.getProperty(COMMON_ENCODE);
-        if (StringUtils.isNotBlank(commonEncode)) {
-            System.setProperty(CLIENT_LOG_ENCODE, commonEncode);
-        } else {
-            String logEncode = System.getProperty(CLIENT_LOG_ENCODE);
-            if (StringUtils.isBlank(logEncode)) {
-                System.setProperty(CLIENT_LOG_ENCODE, CLIENT_LOG_ENCODE_DEFAULT);
-            }
-        }
-    }
-
     public static Logger getLogger(Class<?> clazz) {
         if (clazz == null) {
             return getLogger("");
@@ -81,8 +38,8 @@ public class BoltLoggerFactory {
 
     public static Logger getLogger(String name) {
         if (name == null || name.isEmpty()) {
-            return LoggerSpaceManager.getLoggerBySpace("", BOLT_LOG_SPACE);
+            return LoggerFactory.getLogger("");
         }
-        return LoggerSpaceManager.getLoggerBySpace(name, BOLT_LOG_SPACE);
+        return LoggerFactory.getLogger(name);
     }
 }
